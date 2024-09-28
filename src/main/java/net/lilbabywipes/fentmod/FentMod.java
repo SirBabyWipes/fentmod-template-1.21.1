f87a5a3a5f7a2ede17084b98cee4c9e8781c7e8c
jpsha <jpshan2004@gmail.com>
2024-09-28 18:46:27 -0400

    Initial commit

---
+++ src/main/java/net/lilbabywipes/fentmod/FentMod.java
@@ -0,0 +1,17 @@
+package net.lilbabywipes.fentmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FentMod implements ModInitializer {
+	public static final String MOD_ID = "fentmod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/lilbabywipes/fentmod/FentModClient.java
@@ -0,0 +1,10 @@
+package net.lilbabywipes.fentmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FentModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
