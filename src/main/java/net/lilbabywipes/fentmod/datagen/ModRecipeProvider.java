d3eb9862011f565bcf03da2e2e8897d87c4d3251
jpsha <jpshan2004@gmail.com>
2024-09-30 17:25:19 -0400

    added recipes for joint, crackpipe, and cocaine carpet

--- src/main/java/net/lilbabywipes/fentmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/lilbabywipes/fentmod/datagen/ModRecipeProvider.java
@@ -39,6 +39,16 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .input(ModItems.BONGEMPTY)
                 .criterion(hasItem(ModItems.WEED), conditionsFromItem(ModItems.WEED))
                 .offerTo(exporter);
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.COCAINE_CARPET_BLOCK, 1)
+                .input(ModItems.COCAINE)
+                .input(ModItems.COCAINE)
+                .criterion(hasItem(ModItems.COCAINE), conditionsFromItem(ModItems.COCAINE))
+                .offerTo(exporter);
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.JOINT, 1)
+                .input(ModItems.WEED)
+                .input(Items.PAPER)
+                .criterion(hasItem(ModItems.WEED), conditionsFromItem(ModItems.WEED))
+                .offerTo(exporter);
 
         ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.WEED_SEEDS, 1)
                 .input(ModItems.WEED)
@@ -55,6 +65,15 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .pattern(" # ")
                 .input('#', Items.IRON_INGOT)
                 .criterion(hasItem(Items.IRON_INGOT), conditionsFromItem(Items.IRON_INGOT))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.CRACK_PIPE, 1)
+                .pattern("  #")
+                .pattern("GGG")
+                .pattern("   ")
+                .input('G', Items.GLASS)
+                .input('#', Items.AMETHYST_SHARD)
+                .criterion(hasItem(Items.AMETHYST_SHARD), conditionsFromItem(Items.AMETHYST_SHARD))
                 .offerTo(exporter);
     }
 }
